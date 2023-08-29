a9c5026f9f2039ba959952ac695832c165725329
Lique007 <angeliquehilario019@gmail.com>
2023-08-29 13:47:17 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
- export default function App() {
-  return <div className="App">
-    Weather
-  </div>
-    
-  
+export default function App() {
+  return <div className="App">Weather</div>;
 }
-
-
